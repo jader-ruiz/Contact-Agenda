e0389c2f540c27005a0a55a0c66f4b60cd6e3563
Jader Ruiz <ruizjader.2707@gmail.com>
2026-07-12 10:44:25 -0500

    Create the window with tkinter

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import tkinter as tk
 
 root = tk.Tk()
+
+firstLabel = tk.Label(root, text="Contact Agenda")
+firstLabel.pack()
+root.geometry("700x500")
+root.resizable(False, False)
+
+root.mainloop()
